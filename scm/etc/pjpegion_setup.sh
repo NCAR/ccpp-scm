53f74936bd773a805d4dc8a44e8a9ebd75ffd1ec
Grant Firl <grantf@ucar.edu>
2019-07-17 12:32:12 -0600

    move machine-specific and other compilation flags out of main CMakeLists.txt and into a machine-specific setup script for pjpegion; note that -finit-real=nan is now needed for runs to complete in release mode; check on initializations issues in a followup PR

--- scm/etc/pjpegion_setup.sh
+++ scm/etc/pjpegion_setup.sh
@@ -3,4 +3,4 @@
 echo "Setting environment variables for SCM-CCPP on MACOSX with clang/gfortran"
 
 export CFLAGS="-fmax-stack-var-size=30000 -I /Volumes/Cluster/opt/local/include/"
-export FFLAGS="-fmax-stack-var-size=30000 -fbounds-check -fcheck=all -gdwarf-4 -fvar-tracking-assignments -fbacktrace -finit-real=nan -fcheck=bounds -ffree-line-length-none -cpp -fdefault-real-8 -fdec"
+export FFLAGS="-fmax-stack-var-size=30000 -fbounds-check -fcheck=all -gdwarf-4 -fvar-tracking-assignments -fbacktrace -fcheck=bounds -fdec"
