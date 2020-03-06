268454d6e2994103198bfd918344ed5f17a1f5fa
Grant Firl <grantf@ucar.edu>
2020-03-06 14:04:29 -0700

    move docker machine setup stuff to CENTOS_docker_setup.sh; install NCEPLIBS using new installation script from CENTOS_docker_setup.sh

---
+++ scm/etc/CENTOS_docker_setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Setting environment variables for SCM-CCPP on CENTOS with gcc/gfortran"
+
+export CC=/opt/rh/devtoolset-8/root/usr/bin/gcc
+export CXX=/opt/rh/devtoolset-8/root/usr/bin/g++
+export F77=/opt/rh/devtoolset-8/root/usr/bin/gfortran
+export F90=/opt/rh/devtoolset-8/root/usr/bin/gfortran
+export FC=/opt/rh/devtoolset-8/root/usr/bin/gfortran
+
+export NETCDF=/comsoftware/libs/netcdf
+
+echo/ "Running NCEPLIBS installation script for SCM-CCPP"
+cd ..
+./contrib/build_nceplibs.sh $PWD/nceplibs
+cd scm
+
+echo/ "Setting NCEPLIBS environment variables for SCM-CCPP"
+export BACIO_LIB4=/comsoftware/gmtb-scm/nceplibs/lib/libbacio_v2.2.0_4.a
+export SP_LIBd=/comsoftware/gmtb-scm/nceplibs/lib/libsp_v2.1.0_d.a
+export W3NCO_LIBd=/comsoftware/gmtb-scm/nceplibs/lib/libw3nco_v2.1.0_d.a
+
