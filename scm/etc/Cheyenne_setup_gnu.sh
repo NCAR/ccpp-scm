34dc31efcfbc6b663ba64ebbb53daae33a179063
Grant Firl <grantf@ucar.edu>
2020-03-13 11:35:37 -0600

    trying bash scripts for Cheyenne

--- scm/etc/Cheyenne_setup_gnu.sh
+++ scm/etc/Cheyenne_setup_gnu.sh
@@ -2,29 +2,6 @@
 
 echo "Setting environment variables for SCM-CCPP on Cheyenne with gcc/gfortran"
 
-echo "Setting up python environment for plotting. A NCAR Package Library for python will be cloned into /glade/work/$USER."
-module load python/2.7.16
-ncar_pylib
-if [ -d "/glade/work/$USER/gmtb_scm_python_clone"]; then
-    echo "gmtb_scm_python_clone NPL exists. Loading..."
-    ncar_pylib gmtb_scm_python_clone
-else
-    echo "gmtb_scm_python_clone does not exist yet. Creating..."
-    ncar_pylib -c 20190627 /glade/work/$USER/gmtb_scm_python_clone
-    ncar_pylib gmtb_scm_python_clone
-fi
-
-#check to see if f90nml is installed locally
-echo "Checking if f90nml python module is installed"
-python -c "import f90nml"
-
-if [ $? -ne 0 ]; then
-        echo "Not found; installing f90nml"
-        pip install f90nml==0.19
-else
-        echo "f90nml is installed"
-fi
-
 #load the modules in order to compile the GMTB SCM
 echo "Loading gnu and netcdf modules..."
 module purge
@@ -40,9 +17,32 @@ export CXX=g++
 export FC=gfortran
 
 echo "Setting NCEPLIBS environment variables"
-module use -a /glade/p/ral/jntp/GMTB/tools/modulefiles/gnu-8.3.0/mpt-2.19
+module use /glade/p/ral/jntp/GMTB/tools/modulefiles/gnu-8.3.0/mpt-2.19
 module load  NCEPlibs/1.0.0
 
 echo "Loading cmake"
 module load cmake/3.16.4
 export CMAKE_Platform=cheyenne.gnu
+
+echo "Setting up python environment for plotting. A NCAR Package Library for python will be cloned into /glade/work/$USER."
+module load python/2.7.16
+ncar_pylib
+if [ -d "/glade/work/$USER/gmtb_scm_python_clone" ]; then
+    echo "gmtb_scm_python_clone NPL exists. Loading..."
+    ncar_pylib gmtb_scm_python_clone
+else
+    echo "gmtb_scm_python_clone does not exist yet. Creating..."
+    ncar_pylib -c 20190627 /glade/work/$USER/gmtb_scm_python_clone
+    ncar_pylib gmtb_scm_python_clone
+fi
+
+#check to see if f90nml is installed locally
+echo "Checking if f90nml python module is installed"
+python -c "import f90nml"
+
+if [ $? -ne 0 ]; then
+        echo "Not found; installing f90nml"
+        pip install --no-cache-dir f90nml==0.19
+else
+        echo "f90nml is installed"
+fi
--- scm/etc/Cheyenne_setup_intel.sh
+++ scm/etc/Cheyenne_setup_intel.sh
@@ -2,10 +2,32 @@
 
 echo "Setting environment variables for SCM-CCPP on Cheyenne with icc/ifort"
 
+#load the modules in order to compile the GMTB SCM
+echo "Loading intel and netcdf modules..."
+module purge
+module load ncarenv/1.3
+module load intel/19.0.5
+module load mpt/2.19
+module load ncarcompilers/0.5.0
+module load netcdf/4.7.3
+
+echo "Setting CC/CXX/FC environment variables"
+export CC=icc
+export CXX=icpc
+export FC=ifort
+
+echo "Setting NCEPLIBS environment variables"
+module use /glade/p/ral/jntp/GMTB/tools/modulefiles/intel-19.0.5/mpt-2.19
+module load  NCEPlibs/1.0.0
+
+echo "Loading cmake"
+module load cmake/3.16.4
+export CMAKE_Platform=cheyenne.intel
+
 echo "Setting up python environment for plotting. A NCAR Package Library for python will be cloned into /glade/work/$USER."
 module load python/2.7.16
 ncar_pylib
-if [ -d "/glade/work/$USER/gmtb_scm_python_clone"]; then
+if [ -d "/glade/work/$USER/gmtb_scm_python_clone" ]; then
     echo "gmtb_scm_python_clone NPL exists. Loading..."
     ncar_pylib gmtb_scm_python_clone
 else
@@ -23,26 +45,4 @@ if [ $? -ne 0 ]; then
 else
         echo "f90nml is installed"
 fi
-
-#load the modules in order to compile the GMTB SCM
-echo "Loading intel and netcdf modules..."
-module purge
-module load ncarenv/1.3
-module load intel/19.0.5
-module load mpt/2.19
-module load ncarcompilers/0.5.0
-module load netcdf/4.7.3
-
-echo "Setting CC/CXX/FC environment variables"
-export CC=icc
-export CXX=icpc
-export FC=ifort
-
-echo "Setting NCEPLIBS environment variables"
-module use -a /glade/p/ral/jntp/GMTB/tools/modulefiles/intel-19.0.5/mpt-2.19
-module load  NCEPlibs/1.0.0
-
-echo "Loading cmake"
-module load cmake/3.16.4
-export CMAKE_Platform=cheyenne.intel
 
