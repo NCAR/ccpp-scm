2d0c5d6776529153341ea1d1df23164cf5be0507
Grant Firl <grantf@ucar.edu>
2020-03-25 17:44:32 -0600

    update ccpp_prebuild_config.py in accordance with FV3 version; update RRTMGP suite and namelist to be based off SCM_GFS_v15p2 suite; update submodule pointers; add multi_run config for RRTMGP; add basic plot_config for GFS_v15p2 vs RRTMGP

--- ccpp/config/ccpp_prebuild_config.py
+++ ccpp/config/ccpp_prebuild_config.py
@@ -409,7 +409,6 @@ OPTIONAL_ARGUMENTS = {
         },
     'rrtmgp_sw_rte' : {
          'rrtmgp_sw_rte_run' : [
-             'RRTMGP_sw_heating_rate_clear_sky',
              'components_of_surface_downward_shortwave_fluxes',
              'sw_fluxes_sfc',
              'sw_fluxes_toa',
@@ -417,7 +416,6 @@ OPTIONAL_ARGUMENTS = {
          },
     'GFS_rrtmgp_sw_post' : {
          'GFS_rrtmgp_sw_post_run' : [
-             'RRTMGP_sw_heating_rate_clear_sky',
              'components_of_surface_downward_shortwave_fluxes',
              'sw_fluxes_sfc',
              'sw_fluxes_toa',
@@ -425,14 +423,12 @@ OPTIONAL_ARGUMENTS = {
          },
     'rrtmgp_lw_rte' : {
          'rrtmgp_lw_rte_run' : [
-             'RRTMGP_lw_heating_rate_clear_sky',
              'lw_fluxes_sfc',
              'lw_fluxes_toa',
              ],
         },
     'GFS_rrtmgp_lw_post' : {
          'GFS_rrtmgp_lw_post_run' : [
-             'RRTMGP_lw_heating_rate_clear_sky',
              'lw_fluxes_sfc',
              'lw_fluxes_toa',
              ],
