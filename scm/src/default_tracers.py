29d55676a92f366616411d9974bdd5ed82088914
Grant Firl <grantf@ucar.edu>
2020-07-27 09:36:40 -0600

    add YSU, saYSU, and ACM suites to default_tracers.py and default_namelists.py

--- scm/src/default_tracers.py
+++ scm/src/default_tracers.py
@@ -6,4 +6,7 @@ default_tracers = {"SCM_GFS_v15p2":"tracers_gfdlmp.txt",
                    "SCM_GFS_v15p2_noahmp":"tracers_gfdlmp.txt",
                    "SCM_GFS_v15p2_MYJ":"tracers_gfdlmp.txt",
                    "SCM_GSD_v1":"tracers_gsd.txt",
+                   "SCM_GFS_v15p2_YSU":"tracers_gfdlmp.txt",
+                   "SCM_GFS_v15p2_saYSU":"tracers_gfdlmp.txt",
+                   "SCM_GFS_v15p2_ACM":"tracers_gfdlmp.txt"
                   }
