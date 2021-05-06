5e5fca14d270edebd358df3a8b2a3a9c44239335
Grant Firl <grantf@ucar.edu>
2021-05-06 16:54:43 -0600

    update to ccpp-physics master as of 20210506; add -fno-range-check to GNU SCM compiler options (since removed from ccpp-physics and needed for at least mersenne_twister in ccpp-physics)

--- ccpp/config/ccpp_prebuild_config.py
+++ ccpp/config/ccpp_prebuild_config.py
@@ -288,13 +288,6 @@ OPTIONAL_ARGUMENTS = {
             'tendency_of_ice_friendly_aerosols_at_surface',
             ],
         },
-    'mp_fer_hires' : {
-        'mp_fer_hires_init' : [
-            'fraction_of_ice_water_cloud',
-            'fraction_of_rain_water_cloud',
-            'rime_factor',
-            ],
-        },
     'rrtmgp_sw_rte' : {
          'rrtmgp_sw_rte_run' : [
              'components_of_surface_downward_shortwave_fluxes',
