f923c2cc774616f4bc66952c8f632a0d7b538937
Ted Mansell <ted.mansell@noaa.gov>
2022-01-31 21:56:20 -0600

    Although hail is not physically needed for SCM tests, this changes the GSD_v1nssl namelist/tracers settings to turn hail on so that it is tested in the system.

--- scm/src/default_namelists.py
+++ scm/src/default_namelists.py
@@ -1,6 +1,6 @@
 default_physics_namelists = {"SCM_csawmg":"input_csawmg.nml",
                             "SCM_GSD_v1":"input_GSD_v1.nml",
-                            "SCM_GSD_v1nssl":"input_GSD_v1nssl_nohail.nml",
+                            "SCM_GSD_v1nssl":"input_GSD_v1nssl.nml",
                             "SCM_GFS_v15p2":"input_GFS_v15p2.nml",
                             "SCM_GFS_v16":"input_GFS_v16.nml",
                             "SCM_GFS_v15p2_no_nsst":"input_GFS_v15p2.nml",
