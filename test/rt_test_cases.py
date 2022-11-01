c8192793ad990b58b18102270d698bff84f05c0d
dustinswales <dswales@ucar.edu>
2022-11-01 14:57:00 -0600

    Reorg of RT case list.

--- test/rt_test_cases.py
+++ test/rt_test_cases.py
@@ -2,35 +2,30 @@ run_list = [\
             #----------------------------------------------------------------------------------------------------------------------------------------------
             # CCPP-SCM v6 supported suites 
             #----------------------------------------------------------------------------------------------------------------------------------------------
-            {"case": "arm_sgp_summer_1997_A", "suite": "SCM_GFS_v15p2"},                                                                                 \
             {"case": "arm_sgp_summer_1997_A", "suite": "SCM_GFS_v16"},                                                                                   \
             {"case": "arm_sgp_summer_1997_A", "suite": "SCM_GFS_v17_p8"},                                                                                \
             {"case": "arm_sgp_summer_1997_A", "suite": "SCM_HRRR"},                                                                                      \
             {"case": "arm_sgp_summer_1997_A", "suite": "SCM_RRFS_v1beta"},                                                                               \
             {"case": "arm_sgp_summer_1997_A", "suite": "SCM_RAP"},                                                                                       \
             {"case": "arm_sgp_summer_1997_A", "suite": "SCM_WoFS_v0"},                                                                                   \
-            {"case": "twpice",                "suite": "SCM_GFS_v15p2"},                                                                                 \
             {"case": "twpice",                "suite": "SCM_GFS_v16"},                                                                                   \
             {"case": "twpice",                "suite": "SCM_GFS_v17_p8"},                                                                                \
             {"case": "twpice",                "suite": "SCM_HRRR"},                                                                                      \
             {"case": "twpice",                "suite": "SCM_RRFS_v1beta"},                                                                               \
             {"case": "twpice",                "suite": "SCM_RAP"},                                                                                       \
             {"case": "twpice",                "suite": "SCM_WoFS_v0"},                                                                                   \
-            {"case": "bomex",                 "suite": "SCM_GFS_v15p2"},                                                                                 \
             {"case": "bomex",                 "suite": "SCM_GFS_v16"},                                                                                   \
             {"case": "bomex",                 "suite": "SCM_GFS_v17_p8"},                                                                                \
             {"case": "bomex",                 "suite": "SCM_HRRR"},                                                                                      \
             {"case": "bomex",                 "suite": "SCM_RRFS_v1beta"},                                                                               \
             {"case": "bomex",                 "suite": "SCM_RAP"},                                                                                       \
             {"case": "bomex",                 "suite": "SCM_WoFS_v0"},                                                                                   \
-            {"case": "astex",                 "suite": "SCM_GFS_v15p2"},                                                                                 \
             {"case": "astex",                 "suite": "SCM_GFS_v16"},                                                                                   \
             {"case": "astex",                 "suite": "SCM_GFS_v17_p8"},                                                                                \
             {"case": "astex",                 "suite": "SCM_HRRR"},                                                                                      \
             {"case": "astex",                 "suite": "SCM_RRFS_v1beta"},                                                                               \
             {"case": "astex",                 "suite": "SCM_RAP"},                                                                                       \
             {"case": "astex",                 "suite": "SCM_WoFS_v0"},                                                                                   \
-            {"case": "LASSO_2016051812",      "suite": "SCM_GFS_v15p2"},                                                                                 \
             {"case": "LASSO_2016051812",      "suite": "SCM_GFS_v16"},                                                                                   \
             {"case": "LASSO_2016051812",      "suite": "SCM_GFS_v17_p8"},                                                                                \
             {"case": "LASSO_2016051812",      "suite": "SCM_HRRR"},                                                                                      \
@@ -40,6 +35,11 @@ run_list = [\
             #---------------------------------------------------------------------------------------------------------------------------------------------------
             # Unsupported suites (w/ supported cases)
             #--------------------------------------------------------------------------------------------------------------------------------------------------- 
+            {"case": "arm_sgp_summer_1997_A", "suite": "SCM_GFS_v15p2"},                                                                                 \
+            {"case": "twpice",                "suite": "SCM_GFS_v15p2"},                                                                                 \
+            {"case": "bomex",                 "suite": "SCM_GFS_v15p2"},                                                                                 \
+            {"case": "astex",                 "suite": "SCM_GFS_v15p2"},                                                                                 \
+            {"case": "LASSO_2016051812",      "suite": "SCM_GFS_v15p2"},                                                                                 \
             {"case": "arm_sgp_summer_1997_A", "suite": "SCM_GFS_v17_p8_RRTMGP", "namelist": "input_GFS_v17_p8_RRTMGP.nml", "tracer": "tracers_GFS_v17_p8.txt"},\
             {"case": "twpice",                "suite": "SCM_GFS_v17_p8_RRTMGP", "namelist": "input_GFS_v17_p8_RRTMGP.nml", "tracer": "tracers_GFS_v17_p8.txt"},\
             {"case": "bomex",                 "suite": "SCM_GFS_v17_p8_RRTMGP", "namelist": "input_GFS_v17_p8_RRTMGP.nml", "tracer": "tracers_GFS_v17_p8.txt"},\
