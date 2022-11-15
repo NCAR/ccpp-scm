dff07eb3118026e58a88207d448c36ccd1e95b1e
dustinswales <dswales@ucar.edu>
2022-11-14 17:12:09 -0700

    Add step to cache (artifact) SCM RT results.

--- test/rt_ci.py
+++ test/rt_ci.py
@@ -4,13 +4,30 @@ import os
 import sys
 from rt_test_cases import run_list
 from os.path import exists
+import argparse
 
 #
-for run in run_list:
-    file_out = "../scm/run/" + "output_"+run["case"]+"_"+run["suite"]+"/output.nc"
-    if exists(file_out):
-        os.system("mkdir -p artifact/"+run["case"]+"_"+run["suite"]+"/")
-        os.system("cp " + file_out + " artifact/"+run["case"]+"_"+run["suite"]+"/output.nc")
-    else:
-        print("FAIL:  Could not copy output to for baseline generation")
-        exit()
+parser = argparse.ArgumentParser()
+parser.add_argument('-b', '--build_type',  help='SCM build type')
+
+def parse_args():
+    args       = parser.parse_args()
+    build_type = args.build_type
+    return (build_type)
+
+def main():
+    
+    (build_type) = parse_args()
+
+    #
+    for run in run_list:
+        file_out = "../scm/run/" + "output_"+run["case"]+"_"+run["suite"]+"/output.nc"
+        if exists(file_out):
+            os.system("mkdir -p artifact-"+build_type+"/"+run["case"]+"_"+run["suite"]+"/")
+            os.system("cp " + file_out + " artifact-"+build_type+"/"+run["case"]+"_"+run["suite"]+"/output.nc")
+        else:
+            print("FAIL: Could not copy output to for baseline generation")
+            exit()
+
+if __name__ == '__main__':
+    main()
