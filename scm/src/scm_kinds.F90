314cdd25236016f0e553e7e24f32864a71be4676
Soren Rasmussen <s.c.rasmussen@gmail.com>
2023-11-03 17:55:58 -0600

    new feature: single precision SCM build. Adding kind_scm_{dp,qp} to handle building of generic interfaces in single precision.

--- scm/src/scm_kinds.F90
+++ scm/src/scm_kinds.F90
@@ -6,9 +6,17 @@ module scm_kinds
 !! \section arg_table_scm_kinds
 !! \htmlinclude scm_kinds.html
 !!
-
-  integer, parameter :: sp  = selected_real_kind(P= 6,R=37)
-  integer, parameter :: dp  = selected_real_kind(P=13,R=300)
+  integer, parameter :: sp = selected_real_kind(P= 6,R=37)
+#ifndef SINGLE_PREC
+  integer, parameter :: dp = selected_real_kind(P=13,R=300)
   integer, parameter :: qp = selected_real_kind(P=27,R=2400)
+#else
+  integer, parameter :: dp = sp
+  integer, parameter :: qp = sp
+#endif
 
+  ! these types exists to allow generic interface compilation in scm_utils.F90
+  integer, parameter :: kind_scm_sp = selected_real_kind(P=13,R=300)
+  integer, parameter :: kind_scm_dp = selected_real_kind(P=13,R=300)
+  integer, parameter :: kind_scm_qp = selected_real_kind(P=27,R=2400)
 end module scm_kinds
