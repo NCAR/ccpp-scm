1aceb9aa5682d6ca6810e0d77079e1a09f83421a
Michael Kavulich, Jr <kavulich@ucar.edu>
2023-11-16 16:26:05 -0700

    Add netcdf loads to derecho modules. Not sure why this was working for me without them earlier...

--- scm/etc/modules/derecho_intel.lua
+++ scm/etc/modules/derecho_intel.lua
@@ -18,6 +18,8 @@ load("stack-python/3.10.8")
 load("py-f90nml")
 load("py-netcdf4/1.5.8")
 
+load("netcdf-c/4.9.2")
+load("netcdf-fortran/4.6.0")
 load("bacio/2.4.1")
 load("sp/2.3.3")
 load("w3emc")
