35f5e75ec05304f13fbb59be078407b5b4e09985
Soren Rasmussen <s.c.rasmussen@gmail.com>
2024-01-28 17:44:11 -0700

    fixing kind_scm_sp selected_real_kind

--- scm/src/scm_kinds.F90
+++ scm/src/scm_kinds.F90
@@ -16,7 +16,7 @@ module scm_kinds
 #endif
 
   ! these types exists to allow generic interface compilation in scm_utils.F90
-  integer, parameter :: kind_scm_sp = selected_real_kind(P=13,R=300)
+  integer, parameter :: kind_scm_sp = selected_real_kind(P= 6,R=37)
   integer, parameter :: kind_scm_dp = selected_real_kind(P=13,R=300)
   integer, parameter :: kind_scm_qp = selected_real_kind(P=27,R=2400)
 end module scm_kinds
