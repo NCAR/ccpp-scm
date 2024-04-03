6e2c33a8fca006f76ea508c2e7e2745073331af8
Michael Kavulich, Jr. <kavulich@ucar.edu>
2024-04-03 13:42:24 -0600

    Add a macos clang module file

---
+++ scm/etc/modules/macos_clang.lua
@@ -0,0 +1,32 @@
+help([[
+This module loads libraries for building the CCPP Single-Column Model on
+a MacOS machine with clang v15.0.0 compilers
+]])
+
+whatis([===[Loads libraries needed for building the CCPP SCM on a MacOS machine with clang v15.0.0 compilers]===])
+
+local ssd=os.getenv("SPACK_STACK_DIR") or LmodError ("Environment variable SPACK_STACK_DIR is not set")
+prepend_path("MODULEPATH", ssd .. "/envs/scm-test/install/modulefiles/Core")
+
+load("stack-apple-clang/15.0.0")
+load("stack-python/3.10.13")
+load("stack-openmpi/4.1.6")
+
+
+load("cmake/3.28.3")
+
+load("netcdf-c/4.9.2")
+load("netcdf-fortran/4.6.1")
+
+load("py-f90nml/1.4.3")
+load("py-netcdf4/1.5.8")
+
+
+load("bacio/2.4.1")
+load("sp/2.5.0")
+load("w3emc/2.10.0")
+
+setenv("CMAKE_C_COMPILER","clang")
+setenv("CMAKE_CXX_COMPILER","clang++")
+setenv("CMAKE_Fortran_COMPILER","gfortran-12")
+setenv("CMAKE_Platform","macos.clang")
