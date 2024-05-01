3d2ce1bf067cee32dbc3b09ffcd858bf43036bde
Michael Kavulich, Jr <kavulich@ucar.edu>
2024-05-01 10:56:57 -0600

    Add Linux GNU modulefile, remove compiler version from macos_clang modulefile for flexibility

--- scm/etc/modules/linux_gnu.lua
+++ scm/etc/modules/linux_gnu.lua
@@ -1,14 +1,16 @@
 help([[
 This module loads libraries for building the CCPP Single-Column Model on
-a MacOS machine with clang compilers
+a generic Linux machine with GNU compilers. Note that you may have to make
+modifications to this file based on your own compilers and specifics of
+how spack-stack was installed; see the Users Guide for details
 ]])
 
-whatis([===[Loads libraries needed for building the CCPP SCM on a MacOS machine with clang compilers]===])
+whatis([===[Loads libraries needed for building the CCPP SCM on a Linux machine with GNU compilers]===])
 
 local ssd=os.getenv("SPACK_STACK_DIR") or LmodError ("Environment variable SPACK_STACK_DIR is not set")
 prepend_path("MODULEPATH", ssd .. "/envs/scm-test/install/modulefiles/Core")
 
-load("stack-apple-clang")
+load("stack-gnu")
 load("stack-python/3.10.13")
 load("stack-openmpi/4.1.6")
 
@@ -26,7 +28,6 @@ load("bacio/2.4.1")
 load("sp/2.5.0")
 load("w3emc/2.10.0")
 
-setenv("CMAKE_C_COMPILER","clang")
-setenv("CMAKE_CXX_COMPILER","clang++")
-setenv("CMAKE_Fortran_COMPILER","gfortran-12")
-setenv("CMAKE_Platform","macos.clang")
+setenv("CMAKE_C_COMPILER","mpicc")
+setenv("CMAKE_CXX_COMPILER","mpicxx")
+setenv("CMAKE_Fortran_COMPILER","mpif90")
