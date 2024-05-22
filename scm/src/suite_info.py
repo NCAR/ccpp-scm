4f4783db30a025c76808422a097f51a7d8b5c2f0
Michael Kavulich, Jr. <kavulich@ucar.edu>
2024-05-22 12:18:22 -0600

    Rename SCM_GFS_v17_HR3 to magpie, SCM_GFS_v17_HR3_RRTMGP to robin, and RRFS_v1 to warbler

--- scm/src/suite_info.py
+++ scm/src/suite_info.py
@@ -44,10 +44,10 @@ class suite(object):
 suite_list = []
 suite_list.append(suite('SCM_GFS_v16',           'tracers_GFS_v16.txt',                  'input_GFS_v16.nml',                 600.0, 1800.0, True ))
 suite_list.append(suite('SCM_GFS_v17_p8',        'tracers_GFS_v17_p8.txt',               'input_GFS_v17_p8.nml',              600.0, 600.0,  True ))
-suite_list.append(suite('SCM_GFS_v17_HR3',       'tracers_GFS_v17_HR3.txt',              'input_GFS_v17_HR3.nml',             600.0, 600.0,  True ))
-suite_list.append(suite('SCM_GFS_v17_HR3_RRTMGP','tracers_GFS_v17_p8.txt',               'input_GFS_v17_HR3_RRTMGP.nml',      600.0, 600.0,  True ))
+suite_list.append(suite('magpie',       'tracers_GFS_v17_p8.txt',               'input_magpie.nml',             600.0, 600.0,  True ))
+suite_list.append(suite('robin','tracers_GFS_v17_p8.txt',               'input_GFS_v17_HR3_RRTMGP.nml',      600.0, 600.0,  True ))
 suite_list.append(suite('SCM_RAP',               'tracers_RAP.txt',                      'input_RAP.nml',                     600.0, 600.0 , True ))
-suite_list.append(suite('SCM_RRFS_v1',           'tracers_RRFS_v1.txt',                  'input_RRFS_v1.nml',                 600.0, 600.0 , True ))
+suite_list.append(suite('warbler',           'tracers_RRFS_v1.txt',                  'input_RRFS_v1.nml',                 600.0, 600.0 , True ))
 suite_list.append(suite('SCM_RRFS_v1beta',       'tracers_RRFS_v1beta.txt',              'input_RRFS_v1beta.nml',             600.0, 600.0 , True ))
 suite_list.append(suite('SCM_WoFS_v0',           'tracers_WoFS_v0.txt',                  'input_WoFS_v0.nml',                 600.0, 600.0 , True ))
 suite_list.append(suite('SCM_HRRR',              'tracers_HRRR.txt',                     'input_HRRR.nml',                    600.0, 600.0 , True ))
