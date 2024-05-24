1928b13cc1ef979fd50dc20434352b83b9f94e7c
Michael Kavulich, Jr. <kavulich@ucar.edu>
2024-05-24 11:26:43 -0600

    Didn't update regression test list correctly

--- test/rt_test_cases.py
+++ test/rt_test_cases.py
@@ -6,34 +6,26 @@ run_list = [\
             {"case": "arm_sgp_summer_1997_A", "suite": "magpie"},            \
             {"case": "arm_sgp_summer_1997_A", "suite": "robin"},             \
             {"case": "arm_sgp_summer_1997_A", "suite": "warbler"},           \
-            {"case": "arm_sgp_summer_1997_A", "suite": "bluebird"},          \
-            {"case": "arm_sgp_summer_1997_A", "suite": "bald_eagle"},        \
-            {"case": "arm_sgp_summer_1997_A", "suite": "pigeon"},            \
-            {"case": "arm_sgp_summer_1997_A", "suite": "albatross"},         \
             {"case": "arm_sgp_summer_1997_A", "suite": "hummingbird"},       \
             {"case": "twpice",                "suite": "raven"},             \
-            {"case": "twpice",                "suite": "bluebird"},          \
-            {"case": "twpice",                "suite": "bald_eagle"},        \
-            {"case": "twpice",                "suite": "pigeon"},            \
-            {"case": "twpice",                "suite": "albatross"},         \
+            {"case": "twpice",                "suite": "magpie"},            \
+            {"case": "twpice",                "suite": "robin"},             \
+            {"case": "twpice",                "suite": "warbler"},           \
             {"case": "twpice",                "suite": "hummingbird"},       \
             {"case": "bomex",                 "suite": "raven"},             \
-            {"case": "bomex",                 "suite": "bluebird"},          \
-            {"case": "bomex",                 "suite": "bald_eagle"},        \
-            {"case": "bomex",                 "suite": "pigeon"},            \
-            {"case": "bomex",                 "suite": "albatross"},         \
+            {"case": "bomex",                 "suite": "magpie"},            \
+            {"case": "bomex",                 "suite": "robin"},             \
+            {"case": "bomex",                 "suite": "warbler"},           \
             {"case": "bomex",                 "suite": "hummingbird"},       \
             {"case": "astex",                 "suite": "raven"},             \
-            {"case": "astex",                 "suite": "bluebird"},          \
-            {"case": "astex",                 "suite": "bald_eagle"},        \
-            {"case": "astex",                 "suite": "pigeon"},            \
-            {"case": "astex",                 "suite": "albatross"},         \
+            {"case": "astex",                 "suite": "magpie"},            \
+            {"case": "astex",                 "suite": "robin"},             \
+            {"case": "astex",                 "suite": "warbler"},           \
             {"case": "astex",                 "suite": "hummingbird"},       \
             {"case": "LASSO_2016051812",      "suite": "raven"},             \
-            {"case": "LASSO_2016051812",      "suite": "bluebird"},          \
-            {"case": "LASSO_2016051812",      "suite": "bald_eagle"},        \
-            {"case": "LASSO_2016051812",      "suite": "pigeon"},            \
-            {"case": "LASSO_2016051812",      "suite": "albatross"},         \
+            {"case": "LASSO_2016051812",      "suite": "magpie"},            \
+            {"case": "LASSO_2016051812",      "suite": "robin"},             \
+            {"case": "LASSO_2016051812",      "suite": "warbler"},           \
             {"case": "LASSO_2016051812",      "suite": "hummingbird"},       \
             #---------------------------------------------------------------
             # Unsupported suites (w/ supported cases)
