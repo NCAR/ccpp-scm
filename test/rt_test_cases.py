804758fdfb20b06b035af5de5cd6fb391d15cd31
dustinswales <dswales@ucar.edu>
2024-07-16 08:33:27 -0600

    Update SDF names in test_cases

--- test/rt_test_cases.py
+++ test/rt_test_cases.py
@@ -2,28 +2,28 @@ run_list = [\
             #----------------------------------------------------------------------------------------------------------------------------------------------
             # Supported suites for CCPP Version 7 release
             #----------------------------------------------------------------------------------------------------------------------------------------------
-            {"case": "arm_sgp_summer_1997_A", "suite": "SCM_GFS_v17_HR3"},                                                                                \
-            {"case": "arm_sgp_summer_1997_A", "suite": "SCM_GFS_v17_HR3_RRTMGP"},                                                                         \
+            {"case": "arm_sgp_summer_1997_A", "suite": "SCM_GFS_v17_p8_ugwpv1"},                                                                          \
+            {"case": "arm_sgp_summer_1997_A", "suite": "SCM_GFS_v16_RRTMGP"},                                                                             \
             {"case": "arm_sgp_summer_1997_A", "suite": "SCM_GFS_v16"},                                                                                    \
             {"case": "arm_sgp_summer_1997_A", "suite": "SCM_WoFS_v0"},                                                                                    \
             {"case": "arm_sgp_summer_1997_A", "suite": "SCM_RRFS_v1"},                                                                                    \
-            {"case": "twpice",                "suite": "SCM_GFS_v17_HR3"},                                                                                \
-            {"case": "twpice",                "suite": "SCM_GFS_v17_HR3_RRTMGP"},                                                                         \
+            {"case": "twpice",                "suite": "SCM_GFS_v17_p8_ugwpv1"},                                                                          \
+            {"case": "twpice",                "suite": "SCM_GFS_v16_RRTMGP"},                                                                             \
             {"case": "twpice",                "suite": "SCM_GFS_v16"},                                                                                    \
             {"case": "twpice",                "suite": "SCM_WoFS_v0"},                                                                                    \
             {"case": "twpice",                "suite": "SCM_RRFS_v1"},                                                                                    \
-            {"case": "bomex",                 "suite": "SCM_GFS_v17_HR3"},                                                                                \
-            {"case": "bomex",                 "suite": "SCM_GFS_v17_HR3_RRTMGP"},                                                                         \
+            {"case": "bomex",                 "suite": "SCM_GFS_v17_p8_ugwpv1"},                                                                          \
+            {"case": "bomex",                 "suite": "SCM_GFS_v16_RRTMGP"},                                                                             \
             {"case": "bomex",                 "suite": "SCM_GFS_v16"},                                                                                    \
             {"case": "bomex",                 "suite": "SCM_WoFS_v0"},                                                                                    \
             {"case": "bomex",                 "suite": "SCM_RRFS_v1"},                                                                                    \
-            {"case": "astex",                 "suite": "SCM_GFS_v17_HR3"},                                                                                \
-            {"case": "astex",                 "suite": "SCM_GFS_v17_HR3_RRTMGP"},                                                                         \
+            {"case": "astex",                 "suite": "SCM_GFS_v17_p8_ugwpv1"},                                                                          \
+            {"case": "astex",                 "suite": "SCM_GFS_v16_RRTMGP"},                                                                             \
             {"case": "astex",                 "suite": "SCM_GFS_v16"},                                                                                    \
             {"case": "astex",                 "suite": "SCM_WoFS_v0"},                                                                                    \
             {"case": "astex",                 "suite": "SCM_RRFS_v1"},                                                                                    \
-            {"case": "LASSO_2016051812",      "suite": "SCM_GFS_v17_HR3"},                                                                                \
-            {"case": "LASSO_2016051812",      "suite": "SCM_GFS_v17_HR3_RRTMGP"},                                                                         \
+            {"case": "LASSO_2016051812",      "suite": "SCM_GFS_v17_p8_ugwpv1"},                                                                          \
+            {"case": "LASSO_2016051812",      "suite": "SCM_GFS_v16_RRTMGP"},                                                                             \
             {"case": "LASSO_2016051812",      "suite": "SCM_GFS_v16"},                                                                                    \
             {"case": "LASSO_2016051812",      "suite": "SCM_WoFS_v0"},                                                                                    \
             {"case": "LASSO_2016051812",      "suite": "SCM_RRFS_v1"},                                                                                    \
