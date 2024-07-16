f8d81d8696102f3dbc05fbe2f8649edfad08de51
dustinswales <dswales@ucar.edu>
2024-07-16 09:01:33 -0600

    Another missed change

--- scm/src/supported_suites.py
+++ scm/src/supported_suites.py
@@ -1,1 +1,1 @@
-suites = ["SCM_GFS_v16","SCM_GFS_v16_RRTMGP","SCM_GFS_v17_p8_ugwpv1","SCM_RAP","SCM_RRFS_v1beta","SCM_WoFS_v0"]
+suites = ["SCM_GFS_v16","SCM_GFS_v16_RRTMGP","SCM_GFS_v17_p8_ugwpv1","SCM_RAP","SCM_RRFS_v1","SCM_WoFS_v0"]
