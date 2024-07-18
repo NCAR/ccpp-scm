7093c54c808953e1bf5d88af56b76c2761f903a6
Michael J. Kavulich, Jr <kavulich@ucar.edu>
2024-07-18 18:35:35 +0000

    Upgrade modulefiles for spack-stack 1.6.0

--- scm/etc/modules/jet_intel.lua
+++ scm/etc/modules/jet_intel.lua
@@ -5,25 +5,20 @@ the NOAA RDHPC machine Jet using Intel-2021.5.0
 
 whatis([===[Loads libraries needed for building the CCPP SCM on Jet with Intel compilers ]===])
 
-prepend_path("MODULEPATH", "/lfs4/HFIP/hfv3gfs/spack-stack/modulefiles")
-
-load("cmake/3.26.4")
-load("miniconda/3.9.12")
-
-prepend_path("MODULEPATH", "/lfs4/HFIP/hfv3gfs/role.epic/modulefiles")
-prepend_path("MODULEPATH", "/mnt/lfs4/HFIP/hfv3gfs/role.epic/spack-stack/spack-stack-1.5.1/envs/unified-env/install/modulefiles/Core")
+prepend_path("MODULEPATH","/mnt/lfs4/HFIP/hfv3gfs/role.epic/spack-stack/spack-stack-1.6.0/envs/unified-env-rocky8/install/modulefiles/Core")
 
 load("stack-intel/2021.5.0")
 load("stack-intel-oneapi-mpi/2021.5.1")
-load("stack-python/3.10.8")
+load("stack-python/3.10.13")
 load("py-f90nml")
 load("py-netcdf4/1.5.8")
+load("cmake/3.23.1")
 
 load("netcdf-c/4.9.2")
-load("netcdf-fortran/4.6.0")
+load("netcdf-fortran/4.6.1")
 load("bacio/2.4.1")
-load("sp/2.3.3")
-load("w3emc")
+load("sp/2.5.0")
+load("w3emc/2.10.0")
 
 setenv("CMAKE_C_COMPILER","mpiicc")
 setenv("CMAKE_CXX_COMPILER","mpiicpc")
