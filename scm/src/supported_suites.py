779bddc8b257b65f6c9d33c218cb4c481ade0b82
Michael Kavulich, Jr <kavulich@ucar.edu>
2024-08-02 16:11:34 -0600

    Remove SCM_RAP from supported suites

--- scm/src/supported_suites.py
+++ scm/src/supported_suites.py
@@ -1,2 +1,2 @@
-suites = ["SCM_GFS_v16","SCM_GFS_v16_RRTMGP","SCM_GFS_v17_p8_ugwpv1","SCM_RAP","SCM_HRRR_gf","SCM_WoFS_v0"]
+suites = ["SCM_GFS_v16","SCM_GFS_v16_RRTMGP","SCM_GFS_v17_p8_ugwpv1","SCM_HRRR_gf","SCM_WoFS_v0"]
 
