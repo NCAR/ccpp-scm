94f561941bf177ef3fa4a915f7198b48b5fde0d3
dustinswales <dswales@ucar.edu>
2026-02-09 20:44:58 -0700

    Update CI

--- test/verify-mpi.sh
+++ test/verify-mpi.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Verify Intel MPI is installed and configured correctly
+# Verify MPI is installed and configured correctly
 # Usage: verify-mpi.sh [oneapi|classic]
 
 set +e
@@ -19,7 +19,7 @@ if [ "$COMPILER_TYPE" = "oneapi" ]; then
     test "$CC" = "mpiicx" || { echo "ERROR: CC should be mpiicx but is $CC"; exit 1; }
     test "$CXX" = "mpiicpx" || { echo "ERROR: CXX should be mpiicpx but is $CXX"; exit 1; }
     test "$FC" = "mpiifx" || { echo "ERROR: FC should be mpiifx but is $FC"; exit 1; }
-elif ["$COMPILER_TYPE" = "gfortran"]; then
+else
     for compiler in mpicc mpicxx mpif90; do
         which $compiler || { echo "ERROR: $compiler not found"; }
     done
@@ -30,17 +30,6 @@ elif ["$COMPILER_TYPE" = "gfortran"]; then
     test "$CC" = "mpicc"   || { echo "ERROR: CC should be mpicc but is $CC"; exit 1; }
     test "$CXX" = "mpicxx" || { echo "ERROR: CXX should be mpicxx but is $CXX"; exit 1; }
     test "$FC" = "mpif90"  || { echo "ERROR: FC should be mpif90 but is $FC"; exit 1; }    
-else
-    for compiler in mpiicc mpiicx mpiifort; do
-        which $compiler || { echo "ERROR: $compiler not found"; }
-    done
-    mpiicc --version
-    echo "CC: $CC , should be mpiicc"
-    echo "CXX: $CXX , should be mpiicpc"
-    echo "FC: $FC , should be mpiifort"
-    test "$CC" = "mpiicc" || { echo "ERROR: CC should be mpiicc but is $CC"; exit 1; }
-    test "$CXX" = "mpiicpc" || { echo "ERROR: CXX should be mpiicpc but is $CXX"; exit 1; }
-    test "$FC" = "mpiifort" || { echo "ERROR: FC should be mpiifort but is $FC"; exit 1; }
 fi
 
 which mpirun
